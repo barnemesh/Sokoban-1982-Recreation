5bcf974895a039e7eeba4efddd7dd9a960e24ebe
Bar Nemesh <bar.nemesh@cs.huji.ac.il>
2021-11-30 08:26:24 +0200

    Non-tilemap tests

--- Assets/Mechanics/Reset Level/ResetController.cs
+++ Assets/Mechanics/Reset Level/ResetController.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 /**
  * Manager to allow resetting all objects to initial position.
