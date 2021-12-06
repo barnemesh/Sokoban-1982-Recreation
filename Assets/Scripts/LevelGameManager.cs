46f2409fd0737e9482c6b372a4891ac7734bbcef
Bar Nemesh <bar.nemesh@cs.huji.ac.il>
2021-12-06 18:47:29 +0200

    added max resets functionality. added some todos about managers

--- Assets/Scripts/LevelGameManager.cs
+++ Assets/Scripts/LevelGameManager.cs
@@ -14,6 +14,9 @@ public class LevelGameManager : MonoBehaviour
     private GameObject resetText;
 
     [SerializeField]
+    private GameObject loseText;
+
+    [SerializeField]
     private int levelNumber;
 
     [SerializeField]
@@ -30,16 +33,19 @@ public class LevelGameManager : MonoBehaviour
 
     #endregion
 
+
     #region Private Fields
 
-    private static LevelGameManager _shared;
+    // todo: create a "GameManager" that creates LevelManager based on scene? and will be in control of F1 functions
+    private static LevelGameManager _shared; // todo: remove from static?
+    private static int _resets;
     private int _targetCounter;
     private Movement _player;
     private bool _waitingForInput;
     private int _targetScene;
-    private int _resets = 0;
 
     #endregion
+
 
     #region Properties
 
@@ -57,70 +63,87 @@ public class LevelGameManager : MonoBehaviour
      */
     public static int TargetCounter
     {
+        // todo: if no longer static, find a different solution?
         get => _shared._targetCounter;
         set
         {
             _shared._targetCounter = value;
-            if (_shared._targetCounter == 0) Debug.Log("Won. use f1 to do stuff.");
+            if ( _shared._targetCounter == 0 ) Debug.Log("Won. use f1 to do stuff.");
         }
     }
 
     #endregion
 
+
     #region Monobehaviour
 
-    private void Awake()
+    private void Awake ()
     {
         _shared = this;
         _targetScene = levelNumber;
     }
 
-    private void Start()
+    private void Start ()
     {
         resetText.SetActive(false);
         winText.SetActive(false);
+        loseText.SetActive(false);
 
         Instantiate(playerPrefab, levelData.player, Quaternion.identity);
-
+        // todo: count targets here and not at box start
+        // todo: if not static, give boxes the manager as variable?
         GameObject box = new GameObject("Boxes");
-        foreach (var boxPosition in levelData.boxes)
+        foreach ( var boxPosition in levelData.boxes )
         {
             Instantiate(boxPrefab, boxPosition, Quaternion.identity, box.transform);
         }
     }
 
-    private void Update()
+    private void Update ()
     {
         // Use f1 to do stuff.
-        if (!_waitingForInput && Input.GetKeyDown(KeyCode.F1))
+        if ( !_waitingForInput && Input.GetKeyDown(KeyCode.F1) )
         {
             _waitingForInput = true;
             _player.Pause = true;
-            switch (_targetCounter)
+            if ( _targetCounter == 0 )
             {
-                case 0:
-                    winText.SetActive(true);
-                    _targetScene = (levelNumber + 1) % SceneManager.sceneCountInBuildSettings;
-                    break;
-                default:
-                    resetText.SetActive(true);
-                    _targetScene = levelNumber;
-                    break;
+                winText.SetActive(true);
+                _targetScene = (levelNumber + 1) % SceneManager.sceneCountInBuildSettings;
+            }
+            else if ( _resets == maxResetCount )
+            {
+                loseText.SetActive(true);
+                _targetScene = 0;
+            }
+            else
+            {
+                resetText.SetActive(true);
+                _targetScene = levelNumber;
             }
         }
 
         // if already waiting for input, check if there is input.
-        if (!_waitingForInput) return;
-        if (Input.GetKeyDown(KeyCode.Q)) Application.Quit();
+        if ( !_waitingForInput ) return;
+        if ( Input.GetKeyDown(KeyCode.Q) )
+        {
+            Application.Quit();
+        }
 
-        if (Input.GetKeyDown(KeyCode.Y)) SceneManager.LoadScene(_targetScene);
+        if ( Input.GetKeyDown(KeyCode.Y) )
+        {
+            _resets = _targetScene == levelNumber ? _resets + 1 : 0;
+            print(_resets);
+            SceneManager.LoadScene(_targetScene);
+        }
 
-        if (Input.GetKeyDown(KeyCode.N))
+        if ( Input.GetKeyDown(KeyCode.N) )
         {
             _waitingForInput = false;
             _player.Pause = false;
             winText.SetActive(false);
             resetText.SetActive(false);
+            loseText.SetActive(false);
         }
     }
 
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -15,6 +15,7 @@ public class Movement : MonoBehaviour
 
     #endregion
 
+
     #region Inspector
 
     [SerializeField]
@@ -24,7 +25,7 @@ public class Movement : MonoBehaviour
     // [SerializeField]
     // [Tooltip("Up, Down, Left, Right")]
     // private Sprite[] sprites;
-    
+
     [System.Serializable]
     public struct Sprites
     {
@@ -55,66 +56,67 @@ public class Movement : MonoBehaviour
 
     #endregion
 
+
     #region Monobehaviour
 
-    private void Start()
+    private void Start ()
     {
         _lastPosition = myRigidbody.position;
         LevelGameManager.Player = this;
     }
 
     // Update is called once per frame
-    private void Update()
+    private void Update ()
     {
-        if (Pause || _moving) return;
+        if ( Pause || _moving ) return;
 
         // Choose movement direction based on input, i not already moving.
         _targetDirection = Vector2.zero;
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if ( Input.GetKeyDown(KeyCode.LeftArrow) )
         {
             mySpriteRenderer.sprite = _sprites.left;
             _targetDirection = Vector2.left;
         }
 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if ( Input.GetKeyDown(KeyCode.UpArrow) )
         {
             mySpriteRenderer.sprite = _sprites.up;
             _targetDirection = Vector2.up;
         }
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if ( Input.GetKeyDown(KeyCode.RightArrow) )
         {
             mySpriteRenderer.sprite = _sprites.right;
             _targetDirection = Vector2.right;
         }
 
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if ( Input.GetKeyDown(KeyCode.DownArrow) )
         {
             mySpriteRenderer.sprite = _sprites.down;
             _targetDirection = Vector2.down;
         }
 
-        if (_targetDirection == Vector2.zero) return;
+        if ( _targetDirection == Vector2.zero ) return;
 
         // if we need to move - check if we can move in the desired direction.
 
         var hit = Physics2D.Raycast(myRigidbody.position, _targetDirection, 1.0f);
 
         // if there is a box, check if it can be moved before moving.
-        if (hit.collider != null && hit.collider.CompareTag("Box"))
+        if ( hit.collider != null && hit.collider.CompareTag("Box") )
         {
             var boxControl = hit.collider.GetComponent<BoxControl>();
-            if (!boxControl.TryToMoveInDirection(_targetDirection)) return;
+            if ( !boxControl.TryToMoveInDirection(_targetDirection) ) return;
 
             _moving = true;
         }
 
-        if (hit.collider == null) _moving = true;
+        if ( hit.collider == null ) _moving = true;
     }
 
-    private void FixedUpdate()
+    private void FixedUpdate ()
     {
-        if (Pause || !_moving)
+        if ( Pause || !_moving )
             return;
 
         // If we need to move, use exactly updatesCountInMovement to finish the entire movement.
@@ -123,7 +125,7 @@ public class Movement : MonoBehaviour
 
         myRigidbody.MovePosition(_lastPosition + _distancePercentage * _targetDirection);
 
-        if (!(_distancePercentage >= 1))
+        if ( !(_distancePercentage >= 1) )
             return;
 
         _distancePercentage = 0;
